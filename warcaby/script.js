72b9b8bd1bc72459143e267b431b2189030b1a5e
Tymku <tymon.garbarczyk@gmail.com>
2025-02-06 20:29:36 +0100

    Major changes to the warcaby game engine (it's like half the way to working)

--- warcaby/script.js
+++ warcaby/script.js
@@ -1,17 +1,71 @@
 const blackfig = '<img src=\"blackfig.png\">';
-const whitefig = '<img src=\"whitefig.png\">'
+const whitefig = '<img src=\"whitefig.png\">';
+
+let whosTurn = true;
+let turnText = document.getElementById("turn");
+let turnColor = "Ruch białych";
+
+let idString;
+
+let start;
+let destination;
+let possibleDestination1;
+let possibleDestination2;
+
+function switchTurn(){
+    whosTurn = !whosTurn;
+    console.log(whosTurn);
+
+    if(whosTurn ==  true) {
+        turnColor = "Ruch białych";
+        turnText.innerHTML = turnColor;
+    }
+    else {
+        turnColor = "Ruch czarnych";
+        turnText.innerHTML = turnColor;
+    }
+}
 
 function move(event) {
     if(event.currentTarget.innerHTML == blackfig) {
-        
+        if(whosTurn == true) {
+            alert("Ruch białych!");
+        }
+        else {
+            event.currentTarget.style.backgroundColor = "#00FF00";
+
+            idString = event.currentTarget.id;
+            console.log(idString);
+            console.log(idString.length);
+
+            start = Number(idString);
+            console.log(start);
+      
+
+
+        }
     }
     else if(event.currentTarget.innerHTML == whitefig) {
+        if(whosTurn == false) {
+            alert("Ruch czarnych!");
+        }
+        else {
+            event.currentTarget.style.backgroundColor = "#00FF00";
 
+            idString = event.currentTarget.id;
+            console.log(idString);
+            console.log(idString.length);
+        
+            start = Number(idString);
+            console.log(start);
+
+            
+        }
     }
     else {
         alert("Na tym polu nie ma pionka!");
     }
-   console.log(event.currentTarget);
+   console.log(event.currentTarget.innerHTML);
 }
 
 const cells = document.querySelectorAll(".field");
@@ -21,5 +75,3 @@ console.log(cells, cells[0]);
 cells.forEach(function(cell){
     cell.addEventListener("click", move);
 });
-
-
