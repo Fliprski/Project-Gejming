4fb03547477104ba0bdeea5f04ffcdb0d3267a7c
Fliprski_ <fradgowski@gmail.com>
2025-04-08 05:26:46 +0200

    Tried to fix this mess.

--- warcaby/script.js
+++ warcaby/script.js
@@ -1,6 +1,14 @@
 const blackfig = '<img src=\"blackfig.png\">';
 const whitefig = '<img src=\"whitefig.png\">';
 
+const cells = document.querySelectorAll(".field");
+
+//console.log(cells, cells[0]);
+
+cells.forEach(function(cell){
+    cell.addEventListener("click", selectCell);
+});
+
 let whosTurn = true;
 let turnText = document.getElementById("turn");
 let turnColor = "Ruch białych";
@@ -12,6 +20,8 @@ let destination;
 let possibleDestination1;
 let possibleDestination2;
 
+let selected = false;
+
 function switchTurn(){
     whosTurn = !whosTurn;
     console.log(whosTurn);
@@ -19,59 +29,62 @@ function switchTurn(){
     if(whosTurn ==  true) {
         turnColor = "Ruch białych";
         turnText.innerHTML = turnColor;
+        return;
     }
-    else {
-        turnColor = "Ruch czarnych";
-        turnText.innerHTML = turnColor;
-    }
+    turnColor = "Ruch czarnych";
+    turnText.innerHTML = turnColor;
+
+    selected = false;
 }
 
-function move(event) {
-    if(event.currentTarget.innerHTML == blackfig) {
+function selectCell(e) {
+    if(e.currentTarget.innerHTML == blackfig) {
         if(whosTurn == true) {
             alert("Ruch białych!");
         }
         else {
-            event.currentTarget.style.backgroundColor = "#00FF00";
+            e.currentTarget.style.backgroundColor = "#00FF00";
 
-            idString = event.currentTarget.id;
-            console.log(idString);
-            console.log(idString.length);
-
-            start = Number(idString);
-            console.log(start);
+            convertCell(e);
       
-
-
+            selected = true;
         }
     }
-    else if(event.currentTarget.innerHTML == whitefig) {
+    else if(e.currentTarget.innerHTML == whitefig) {
         if(whosTurn == false) {
             alert("Ruch czarnych!");
         }
         else {
-            event.currentTarget.style.backgroundColor = "#00FF00";
+            e.currentTarget.style.backgroundColor = "#00FF00";
 
-            idString = event.currentTarget.id;
-            console.log(idString);
-            console.log(idString.length);
-        
-            start = Number(idString);
-            console.log(start);
+            let y = e.target.parentElement.parentElement.rowIndex - 1;
+            let x = e.target.parentElement.cellIndex - 1;
 
-            
+            move(convertCell(x, y));
+
+            selected = true;
         }
-    }
-    else {
+    }else if(!selected){
         alert("Na tym polu nie ma pionka!");
     }
-   console.log(event.currentTarget.innerHTML);
+   console.log(e.currentTarget.innerHTML);
 }
 
-const cells = document.querySelectorAll(".field");
+function convertCell(x, y){
+    return (8 * y + x);
+}
 
-console.log(cells, cells[0]);
-
-cells.forEach(function(cell){
-    cell.addEventListener("click", move);
-});
+function move(perfectCell){
+    for(let i = -1; i <= 1; i++){
+        if(i == 0) continue;
+        for(let j = -1; j <= 1; j++){
+            if(j == 0) continue;
+            console.log(i, j);
+            /* let subjectCell = perfectCell + convertCell(j, i);
+            console.log(subjectCell, i, j, perfectCell);
+            if(cells[subjectCell].innerHTML == ''){
+                cells[subjectCell].style.backgroundColor = '#69B00B'
+            } */
+        }
+    }
+}
